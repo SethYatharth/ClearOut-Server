baec96d335690971547b6cb6a13dbc969e6eceb9
SethYatharth <yatahrthseth123@gmail.com>
2024-11-04 23:13:58 +0530

    final commit of ClearOut Server

--- src/main/java/com/clearout/config/SecurityConfiguration.java
+++ src/main/java/com/clearout/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.clearout.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
